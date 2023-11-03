f0d39f609eaeb516a5575b08b513ec3e4b86730f
MoonWalker <silavsale@gmail.com>
2023-11-02 22:36:06 -0400

    added solution for 88 Merge Sorted Array

---
+++ Playground/Prototypes.ts
@@ -0,0 +1,9 @@
+const animal = {
+    type: 'monkey',
+    numLegs: 2
+}
+
+console.log(animal);
+console.log(animal.numLegs);
+console.log(animal.toString());
+console.log(animal.hasOwnProperty('toStrings'));
